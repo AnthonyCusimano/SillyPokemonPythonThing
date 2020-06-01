bb6d86632c564e4b91cdbfca2f21a745cda0f9a8
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-01 00:35:53 -0400

    Update Moves.py
    jumpluff's moves are in LOLE

--- Moves.py
+++ Moves.py
@@ -11,7 +11,16 @@ class Move:
     def __init__(self, _idNum):
         self.myIDNum = _idNum
 
-        if self.myIDNum == 79:
+        if self.myIDNum == 73:
+            self.myName = "Leech Seed"
+
+            self.myType = 3 #grass
+            self.myDamageType = 0
+
+            self.myBasePower = 0
+            self.myAccuracy = 90
+
+        elif self.myIDNum == 79:
             self.myName = "sleep powder"
 
             self.myType = 3  # grass
@@ -20,8 +29,17 @@ class Move:
             self.myBasePower = 0
             self.myAccuracy = 75
 
+        elif self.myIDNum == 369:
+            self.myName = "U-turn"
+
+            self.myType = 6  # bug
+            self.myDamageType = 1  # physical
+
+            self.myBasePower = 70
+            self.myAccuracy = 100
+
         elif self.myIDNum == 512:
-            self.myName == "acrobatics"
+            self.myName = "acrobatics"
 
             self.myBasePower = 55
 
