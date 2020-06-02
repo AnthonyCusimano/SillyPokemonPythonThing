c911af79adfe3bfffc2e73332cf7199871defb9e
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-02 01:00:28 -0400

    Update PokemonInfo.py
    Whimsicott added

--- PokemonInfo.py
+++ PokemonInfo.py
@@ -1,11 +1,11 @@
-from Moves import *
+from Moves import Move
 
 class Pokemon:
     myName = ''
     myIDNum = 0
 
     myTypes = [0, 0]
-    myMoves = Move[0, 0, 0, 0]
+    myMoves = [0, 0, 0, 0]
 
     myBaseStats = [0, 0, 0, 0, 0, 0]
 
@@ -13,7 +13,7 @@ class Pokemon:
         self.myName = _name
 
         if self.myName == "Jumpluff":
-            #grass flying
+            #flying grass
             self.myTypes[0] = 2
             self.myTypes[1] = 11
 
@@ -21,3 +21,13 @@ class Pokemon:
             self.myMoves[1] = Move(79)
             self.myMoves[2] = Move(369)
             self.myMoves[3] = Move(512)
+
+        elif self.myName == "Whimsicott":
+            #grass fairy
+            self.myTypes[0] = 11
+            self.myTypes[1] = 17
+
+            self.myMoves[0] = Move(585)
+            self.myMoves[1] = Move(412)
+            self.myMoves[2] = Move(94)
+            self.myMoves[3] = Move(369)
