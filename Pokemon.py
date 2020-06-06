9f87fb7ff4b9fadae81d0511d60a3470ef2098c7
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-06 18:02:23 -0400

    Update Pokemon.py
    found out all my pokemon have the same moves :grimacing: gonna try and figure out why that is tomorrow

--- Pokemon.py
+++ Pokemon.py
@@ -1,14 +1,30 @@
-from PokemonInfo import *
+# from PokemonInfo import *
+from Trainer import *
 
 Jumpy = Pokemon("Jumpluff")
 Whimsy = Pokemon("Whimsicott")
 Wrath = Pokemon("Poliwrath")
 
+
 print(Jumpy.myName)
+print(Jumpy.myMoves[0].myName)
+print(Jumpy.myMoves[1].myName)
+print(Jumpy.myMoves[2].myName)
 print(Jumpy.myMoves[3].myName)
 
-print(Whimsy.myName)
-print(Whimsy.myMoves[2].myName)
 
+print()
+print(Whimsy.myName)
+print(Whimsy.myMoves[0].myName)
+print(Whimsy.myMoves[1].myName)
+print(Whimsy.myMoves[2].myName)
+print(Whimsy.myMoves[3].myName)
+
+
+print()
 print(Wrath.myName)
+print(Wrath.myMoves[0].myName)
+print(Wrath.myMoves[1].myName)
 print(Wrath.myMoves[2].myName)
+print(Wrath.myMoves[3].myName)
+
