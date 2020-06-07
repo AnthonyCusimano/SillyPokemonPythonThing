27ef8e58f3d9cebfa931e7422cf260f54ace1cbe
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-07 13:13:27 -0400

    Update PokemonInfo.py
    found out difference in syntax between class level and object variables :clown_face:

--- PokemonInfo.py
+++ PokemonInfo.py
@@ -2,32 +2,34 @@ from Moves import Move
 
 
 class Pokemon:
-    myName = ''
-    myIDNum = 0
-
-    myTypes = [0, 0]
-    myMoves = [0, 0, 0, 0]
-
-    myBaseStats = [0, 0, 0, 0, 0, 0]
 
     def __init__(self, _name):
+
+        # myBaseStats = [0, 0, 0, 0, 0, 0]
+
         self.myName = _name
+        self.myIDNum = 0
+
+        self.myTypes = [0, 0]
+        self.myMoves = [0, 0, 0, 0]
 
         if self.myName == "Poliwrath":
             # water fighting
             self.myTypes[0] = 10
             self.myTypes[1] = 1
 
+            print("Poliwrath moves!!!!")
             self.myMoves[0] = Move(503)
             self.myMoves[1] = Move(509)
             self.myMoves[2] = Move(95)
-            self.myMoves[3] = Move (182)
+            self.myMoves[3] = Move(182)
 
         elif self.myName == "Jumpluff":
             # flying grass
             self.myTypes[0] = 2
             self.myTypes[1] = 11
 
+            print("Jumpluff moves!!!!")
             self.myMoves[0] = Move(73)
             self.myMoves[1] = Move(79)
             self.myMoves[2] = Move(369)
@@ -38,6 +40,7 @@ class Pokemon:
             self.myTypes[0] = 11
             self.myTypes[1] = 17
 
+            print("Whimsicott moves!!!!")
             self.myMoves[0] = Move(585)
             self.myMoves[1] = Move(412)
             self.myMoves[2] = Move(94)
