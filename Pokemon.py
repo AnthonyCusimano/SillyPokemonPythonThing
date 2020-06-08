c8831704fc94bd5aa977528a50d58b439fa03cc0
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-07 20:35:32 -0400

    Update Pokemon.py
    hitmonlee diff

--- Pokemon.py
+++ Pokemon.py
@@ -1,17 +1,18 @@
 # from PokemonInfo import *
 from Trainer import *
+# from Moves import Move
 
 Jumpy = Pokemon("Jumpluff")
 Whimsy = Pokemon("Whimsicott")
-Wrath = Pokemon("Poliwrath")
 
+Wrath = Pokemon("Poliwrath")
+Lee = Pokemon("Hitmonlee")
 
 print(Jumpy.myName)
 print(Jumpy.myMoves[0].myName)
 print(Jumpy.myMoves[1].myName)
 print(Jumpy.myMoves[2].myName)
 print(Jumpy.myMoves[3].myName)
-
 
 print()
 print(Whimsy.myName)
@@ -20,7 +21,6 @@ print(Whimsy.myMoves[1].myName)
 print(Whimsy.myMoves[2].myName)
 print(Whimsy.myMoves[3].myName)
 
-
 print()
 print(Wrath.myName)
 print(Wrath.myMoves[0].myName)
@@ -28,3 +28,9 @@ print(Wrath.myMoves[1].myName)
 print(Wrath.myMoves[2].myName)
 print(Wrath.myMoves[3].myName)
 
+print()
+print(Lee.myName)
+print(Lee.myMoves[0].myName)
+print(Lee.myMoves[1].myName)
+print(Lee.myMoves[2].myName)
+print(Lee.myMoves[3].myName)
