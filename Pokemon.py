8c27b3daf4f9ecb1702d25e112a677fa4464c100
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-07 21:26:28 -0400

    Update Pokemon.py
    all 6 pokemon added

--- Pokemon.py
+++ Pokemon.py
@@ -3,16 +3,32 @@ from Trainer import *
 # from Moves import Move
 
 Jumpy = Pokemon("Jumpluff")
-Whimsy = Pokemon("Whimsicott")
-
 Wrath = Pokemon("Poliwrath")
+Mag = Pokemon("Magcargo")
+
+Whimsy = Pokemon("Whimsicott")
 Lee = Pokemon("Hitmonlee")
+Gor = Pokemon("Gorebyss")
 
 print(Jumpy.myName)
 print(Jumpy.myMoves[0].myName)
 print(Jumpy.myMoves[1].myName)
 print(Jumpy.myMoves[2].myName)
 print(Jumpy.myMoves[3].myName)
+
+print()
+print(Mag.myName)
+print(Mag.myMoves[0].myName)
+print(Mag.myMoves[1].myName)
+print(Mag.myMoves[2].myName)
+print(Mag.myMoves[3].myName)
+
+print()
+print(Wrath.myName)
+print(Wrath.myMoves[0].myName)
+print(Wrath.myMoves[1].myName)
+print(Wrath.myMoves[2].myName)
+print(Wrath.myMoves[3].myName)
 
 print()
 print(Whimsy.myName)
@@ -22,11 +38,11 @@ print(Whimsy.myMoves[2].myName)
 print(Whimsy.myMoves[3].myName)
 
 print()
-print(Wrath.myName)
-print(Wrath.myMoves[0].myName)
-print(Wrath.myMoves[1].myName)
-print(Wrath.myMoves[2].myName)
-print(Wrath.myMoves[3].myName)
+print(Gor.myName)
+print(Gor.myMoves[0].myName)
+print(Gor.myMoves[1].myName)
+print(Gor.myMoves[2].myName)
+print(Gor.myMoves[3].myName)
 
 print()
 print(Lee.myName)
