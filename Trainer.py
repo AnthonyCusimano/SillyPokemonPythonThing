1913c058206cff49049e137ecd82c5f47d093383
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-10 19:24:17 -0400

    Update Trainer.py
    added constructor w/ an overload for predetermined team
    added swap ability that will be used in battle to switch out pokermen

--- Trainer.py
+++ Trainer.py
@@ -1,5 +1,17 @@
-from PokemonInfo import Pokemon
+from PokemonInfo import *
+# GITHUB
 
 
 class Trainer:
-    MyTeam = [0, 0, 0, 0, 0, 0]
+
+    def __init__(self):
+        self.myTeam = [0, 0, 0, 0, 0, 0]
+
+    # send Pokemon objects please
+    def __init__(self, _guy1, _guy2, _guy3):
+        self.myTeam = [Pokemon(_guy1), Pokemon(_guy2), Pokemon(_guy3), 0, 0, 0]
+
+    def swap(self, _newGuyAddress):
+        temp = self.myTeam[0]
+        self.myTeam[0] = self.myTeam[_newGuyAddress]
+        self.myTeam[_newGuyAddress] = temp
