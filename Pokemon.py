a1e245828a6691d70b8d229dd15e03481740a04e
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-10 19:23:24 -0400

    Update Pokemon.py
    removed a bunch of debug stuff
    computer and player Trainers added in

--- Pokemon.py
+++ Pokemon.py
@@ -2,51 +2,6 @@
 from Trainer import *
 # from Moves import Move
 
-Jumpy = Pokemon("Jumpluff")
-Wrath = Pokemon("Poliwrath")
-Mag = Pokemon("Magcargo")
+player = Trainer("Jumpluff", "Poliwrath", "Magcargo")
 
-Whimsy = Pokemon("Whimsicott")
-Lee = Pokemon("Hitmonlee")
-Gor = Pokemon("Gorebyss")
-
-print(Jumpy.myName)
-print(Jumpy.myMoves[0].myName)
-print(Jumpy.myMoves[1].myName)
-print(Jumpy.myMoves[2].myName)
-print(Jumpy.myMoves[3].myName)
-
-print()
-print(Mag.myName)
-print(Mag.myMoves[0].myName)
-print(Mag.myMoves[1].myName)
-print(Mag.myMoves[2].myName)
-print(Mag.myMoves[3].myName)
-
-print()
-print(Wrath.myName)
-print(Wrath.myMoves[0].myName)
-print(Wrath.myMoves[1].myName)
-print(Wrath.myMoves[2].myName)
-print(Wrath.myMoves[3].myName)
-
-print()
-print(Whimsy.myName)
-print(Whimsy.myMoves[0].myName)
-print(Whimsy.myMoves[1].myName)
-print(Whimsy.myMoves[2].myName)
-print(Whimsy.myMoves[3].myName)
-
-print()
-print(Gor.myName)
-print(Gor.myMoves[0].myName)
-print(Gor.myMoves[1].myName)
-print(Gor.myMoves[2].myName)
-print(Gor.myMoves[3].myName)
-
-print()
-print(Lee.myName)
-print(Lee.myMoves[0].myName)
-print(Lee.myMoves[1].myName)
-print(Lee.myMoves[2].myName)
-print(Lee.myMoves[3].myName)
+computer = Trainer("Whimsicott", "Hitmonlee", "Gorebyss")
