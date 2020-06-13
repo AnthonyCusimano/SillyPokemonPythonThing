51d5c285090434134fd18adf50f5c95a4cf0549f
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-12 21:35:58 -0400

    Update Pokemon.py
    ProcessDamage

--- Pokemon.py
+++ Pokemon.py
@@ -1,7 +1,18 @@
 # from PokemonInfo import *
 from Trainer import *
 # from Moves import Move
+from CombatHandler import *
 
 player = Trainer("Jumpluff", "Poliwrath", "Magcargo")
 
+Gamer = CombatHandler()
+
 computer = Trainer("Whimsicott", "Hitmonlee", "Gorebyss")
+
+# not big on this cus it crashes if you do something stupid
+mover = int(input())
+
+player.selectMove(mover)
+
+Gamer.ProcessDamage(player.myTeam[0], computer.myTeam[0], player.myTeam[0].myMoves[mover])
+
