8fecaa47b2126570fd716cd37570cf55b5acd332
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-21 14:28:13 -0400

    Add files via upload
    adding Natures into the game

---
+++ Nature.py
@@ -0,0 +1,82 @@
+
+
+class Nature:
+
+    def __init__(self, T_ID):
+        self.myID = T_ID
+        self.myName
+
+        if self.myID == 0:
+            self.myName = "Hardy"
+
+        elif self.myID == 1:
+            self.myName = "Lonely"
+
+        elif self.myID == 2:
+            self.myName = "Brave"
+
+        elif self.myID == 3:
+            self.myName = "Adamant"
+
+        elif self.myID == 4:
+            self.myName = "Naughty"
+
+        elif self.myID == 5:
+            self.myName = "Bold"
+
+        elif self.myID == 6:
+            self.myName = "Docile"
+
+        elif self.myID == 7:
+            self.myName = "Relaxed"
+
+        elif self.myID == 8:
+            self.myName = "Impish"
+
+        elif self.myID == 9:
+            self.myName = "Lax"
+
+        elif self.myID == 10:
+            self.myName = "Timid"
+
+        elif self.myID == 11:
+            self.myName = "Hasty"
+
+        elif self.myID == 12:
+            self.myName = "Serious"
+
+        elif self.myID == 13:
+            self.myName = "Jolly"
+
+        elif self.myID == 14:
+            self.myName = "Naive"
+
+        elif self.myID == 15:
+            self.myName = "Modest"
+
+        elif self.myID == 16:
+            self.myName = "Mild"
+
+        elif self.myID == 17:
+            self.myName = "Quiet"
+
+        elif self.myID == 18:
+            self.myName = "Bashful"
+
+        elif self.myID == 19:
+            self.myName = "Rash"
+
+        elif self.myID == 20:
+            self.myName = "Calm"
+
+        elif self.myID == 21:
+            self.myName = "Gentle"
+
+        elif self.myID == 22:
+            self.myName = "Sassy"
+
+        elif self.myID == 23:
+            self.myName = "Careful"
+
+        elif self.myID == 24:
+            self.myName = "Quirky"
