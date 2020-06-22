8d8c5db7d0bb8c1d07e6a97280013436b872e417
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-06-22 15:54:25 -0400

    Update Trainer.py
    crit stage moved to CombatHandler

--- Trainer.py
+++ Trainer.py
@@ -6,14 +6,11 @@ class Trainer:
     def __init__(self):
         self.myTeam = Pokemon[0, 0, 0, 0, 0, 0]
 
-        self.myCritStage = 0
-
     # send Pokemon objects please
     def __init__(self, _guy1, _guy2, _guy3):
         self.myTeam = [Pokemon(_guy1), Pokemon(_guy2), Pokemon(_guy3), 0, 0, 0]
 
     def swap(self, _newGuyAddress):
-        self.myCritStage = 0
         temp = self.myTeam[0]
         self.myTeam[0] = self.myTeam[_newGuyAddress]
         self.myTeam[_newGuyAddress] = temp
