d2a64753aa5e0738792a64dfe1381dcc7724f598
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-07-14 15:38:12 -0400

    Update Pokemon.py
    crashes but we're getting there

--- Pokemon.py
+++ Pokemon.py
@@ -16,9 +16,9 @@ computer = Trainer("Whimsicott", "Hitmonlee", "Gorebyss")
 print(player.myTeam[0].myBaseStats[5])
 
 # not big on this cus it crashes if you do something stupid
-mover = int(input())
+# mover = int(input())
 
-player.selectMove(mover)
+# player.selectMove(mover)
 # replace ProcessDamage calls with "Process turn"
 
 # Gamer.ProcessDamage(player.myTeam[0], computer.myTeam[0], player.myTeam[0].myMoves[mover])
@@ -28,4 +28,8 @@ player.selectMove(mover)
 # Gamer.ProcessDamage(computer.myTeam[0], player.myTeam[0], computer.myTeam[0].myMoves[GamerAI.selectMove()])
 
 while ((player.myTeam[0].myCurrentHealth != 0 and player.myTeam[1].myCurrentHealth != 0 and player.myTeam[2].myCurrentHealth != 0) or (computer.myTeam[0].myCurrentHealth != 0 and computer.myTeam[1].myCurrentHealth != 0 and computer.myTeam[2].myCurrentHealth != 0)):
-    print("infinite monkaS")
+    print("NEW TURN WOW!!!")
+    print("Hit 0 for swapping your goon or 1 to select a move")
+    mover = int(input())
+    moveModifier = int(input())
+    Gamer.ProcessTurn(mover, moveModifier, player, computer, GamerAI, player.myTeam[0], computer.myTeam[0])
