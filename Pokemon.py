6a3272abd20b83ce2dfd5dab7d33073e46acaf91
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-07-14 16:25:13 -0400

    Update Pokemon.py
    TODO tags added
    formatting beginning

--- Pokemon.py
+++ Pokemon.py
@@ -27,9 +27,19 @@ print(player.myTeam[0].myBaseStats[5])
 
 # Gamer.ProcessDamage(computer.myTeam[0], player.myTeam[0], computer.myTeam[0].myMoves[GamerAI.selectMove()])
 
+# TODO
+# you can swap to the same pokemon
+# you cannot select a different action
 while ((player.myTeam[0].myCurrentHealth != 0 and player.myTeam[1].myCurrentHealth != 0 and player.myTeam[2].myCurrentHealth != 0) or (computer.myTeam[0].myCurrentHealth != 0 and computer.myTeam[1].myCurrentHealth != 0 and computer.myTeam[2].myCurrentHealth != 0)):
     print("NEW TURN WOW!!!")
+    print("Enemy health is ", computer.myTeam[0].myCurrentHealth, " our of ", computer.myTeam[0].myBaseStats[0])
+    print("Your health is ", player.myTeam[0].myCurrentHealth, " our of ", player.myTeam[0].myBaseStats[0])
     print("Hit 0 for swapping your goon or 1 to select a move")
     mover = int(input())
+
+    if mover == 0:
+        print("1: ", player.myTeam[1].myName, " ", player.myTeam[1].myCurrentHealth, "/", player.myTeam[1].myBaseStats[0])
+        print("2: ", player.myTeam[2].myName, " ",player.myTeam[2].myCurrentHealth, "/", player.myTeam[2].myBaseStats[0])
+
     moveModifier = int(input())
     Gamer.ProcessTurn(mover, moveModifier, player, computer, GamerAI, player.myTeam[0], computer.myTeam[0])
