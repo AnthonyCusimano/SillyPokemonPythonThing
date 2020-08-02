0e1f1fb3359d84f0046fc4ebd13c70f703b25af5
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-08-02 11:35:03 -0400

    Update Items.py
    couldn't find a list of ID numbers for items so w/e we'll do it ourselves. red card added, will have to add an Item listener sort of deal to the CombatHandler

--- Items.py
+++ Items.py
@@ -1,12 +1,16 @@
 
+
+# 
 class Item:
 
     def __init__(self, _name):
         self.myName = _name
-        self.myIDNumber = 0
+
+        if self.myName == "Red Card":
+            self.myIDNumber = 1
 
     def ProcessMyEndOfTurnEffect(self, _myHolder):
         print("Item end of turn item effect")
 
-    def ProcessMyOnAttackedEffect(self, _attack, _myHolder):
+    def ProcessMyOnAttackedEffect(self, _attack, _myHolder, _attacker, _attackerTrainer):
         print("Item on attacked effect")
