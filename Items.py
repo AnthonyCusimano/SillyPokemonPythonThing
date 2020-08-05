301383d70645ceeec6ee585b0d3d449cf7693327
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-08-05 19:26:53 -0400

    Update Items.py
    adding items in, leftovers needs testing

--- Items.py
+++ Items.py
@@ -1,6 +1,16 @@
+import math
 
 
-# 
+def restorePokemonHealth (_pokemon, _healthRestored):
+
+    if _pokemon.myCurrentHealth + _healthRestored > _pokemon.myBaseStats[0]:
+        _pokemon.myCurrentHealth = _pokemon.myBaseStats[0]
+
+    else:
+        _pokemon.myCurrentHealth += _healthRestored
+
+
+#
 class Item:
 
     def __init__(self, _name):
@@ -9,8 +19,14 @@ class Item:
         if self.myName == "Red Card":
             self.myIDNumber = 1
 
+        elif self.myName == "Leftovers":
+            self.myIDNumber = 2
+
     def ProcessMyEndOfTurnEffect(self, _myHolder):
         print("Item end of turn item effect")
+        # leftovers
+        if self.myIDNumber == 2:
+            restorePokemonHealth(_myHolder, math.floor(_myHolder.myBaseStats[0] * 0.0625))
 
     def ProcessMyOnAttackedEffect(self, _attack, _myHolder, _attacker, _attackerTrainer):
         print("Item on attacked effect")
