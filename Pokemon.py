016e54e50ea39ba50b5cc29e7fe08ce9b0ff4863
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-08-11 16:18:43 -0400

    Update Pokemon.py
    changed the way the AI is created

--- Pokemon.py
+++ Pokemon.py
@@ -7,11 +7,11 @@ from CombatHandler import *
 
 player = Trainer("Jumpluff", "Poliwrath", "Magcargo")
 
-GamerAI = BadAIGen1()
-
 Gamer = CombatHandler()
 
 computer = Trainer("Whimsicott", "Hitmonlee", "Gorebyss")
+
+GamerAI = BadAIGen1(computer)
 
 # not big on this cus it crashes if you do something stupid
 # mover = int(input())
@@ -25,11 +25,10 @@ computer = Trainer("Whimsicott", "Hitmonlee", "Gorebyss")
 
 # Gamer.ProcessDamage(computer.myTeam[0], player.myTeam[0], computer.myTeam[0].myMoves[GamerAI.selectMove()])
 
-# TODO
-# you cannot select a different action
-while (player.myTeam[0].myCurrentHealth != 0 and player.myTeam[1].myCurrentHealth != 0 and 
-       player.myTeam[2].myCurrentHealth != 0) or (computer.myTeam[0].myCurrentHealth != 0 and 
-                                                  computer.myTeam[1].myCurrentHealth != 0 and 
+# gamer
+while (player.myTeam[0].myCurrentHealth != 0 and player.myTeam[1].myCurrentHealth != 0 and
+       player.myTeam[2].myCurrentHealth != 0) or (computer.myTeam[0].myCurrentHealth != 0 and
+                                                  computer.myTeam[1].myCurrentHealth != 0 and
                                                   computer.myTeam[2].myCurrentHealth != 0):
     print("NEW TURN WOW!!!")
     print("Enemy health is ", computer.myTeam[0].myCurrentHealth, " our of ", computer.myTeam[0].myBaseStats[0])
@@ -38,9 +37,9 @@ while (player.myTeam[0].myCurrentHealth != 0 and player.myTeam[1].myCurrentHealt
     mover = int(input())
 
     if mover == 0:
-        print("1: ", player.myTeam[1].myName, " ", player.myTeam[1].myCurrentHealth, "/", 
+        print("1: ", player.myTeam[1].myName, " ", player.myTeam[1].myCurrentHealth, "/",
               player.myTeam[1].myBaseStats[0])
-        print("2: ", player.myTeam[2].myName, " ", player.myTeam[2].myCurrentHealth, "/", 
+        print("2: ", player.myTeam[2].myName, " ", player.myTeam[2].myCurrentHealth, "/",
               player.myTeam[2].myBaseStats[0])
 
     elif mover == 1:
