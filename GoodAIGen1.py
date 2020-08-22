ae13316b45cbabfdff97fdc8930830e688bca078
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-08-22 17:25:15 -0400

    Update GoodAIGen1.py
    http://wiki.pokemonspeedruns.com/index.php/Pok%C3%A9mon_Red/Blue/Yellow_Trainer_AI
    comment

--- GoodAIGen1.py
+++ GoodAIGen1.py
@@ -2,9 +2,17 @@ from AI import AI
 import random
 
 
-#
+# http://wiki.pokemonspeedruns.com/index.php/Pok%C3%A9mon_Red/Blue/Yellow_Trainer_AI
 class GoodAIGen1(AI):
     pass
 
     def __init__(self):
+        print("")
+
+    #
+    def selectMove(self, _pokemon):
+        print("")
+
+    #
+    def swap(self, _myParty):
         print("")
