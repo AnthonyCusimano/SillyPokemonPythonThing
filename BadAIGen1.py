12ce6dcf6d7526d326804c149656a515449ef182
Anthony Cusimano <anthonycusimano91@hotmail.com>
2020-08-30 21:25:42 -0400

    Update BadAIGen1.py
    myTrainer added
    comment pass

--- BadAIGen1.py
+++ BadAIGen1.py
@@ -4,14 +4,17 @@ import random
 
 # bad gen 1 AI just picked a random move every turn
 # I dunno how switching worked but I'm fairly certain this is accurate
+# http://wiki.pokemonspeedruns.com/index.php/Pok%C3%A9mon_Red/Blue/Yellow_Trainer_AI
 class BadAIGen1(AI):
     pass
 
+    # self.myTrainer = _myTrainer
     def __init__(self, _myTrainer):
         print("bad gen 1 AI created")
         self.movePicked = 0
         self.myTrainer = _myTrainer
 
+    # pick a random move unless we have an active choice item
     def selectMove(self, _pokemon):
 
         if _pokemon.myItem.myIDNumber == 4 and _pokemon.myChoiceMade:
@@ -25,7 +28,7 @@ class BadAIGen1(AI):
     def swap(self, _myParty):
         # for x in _myParty:
         for x in range(4):
-            if _myParty[x].myCurrentHealth > 0:
-                temp = self.myTeam[0]
-                self.myTeam[0] = _myParty[x]
+            if self.myTrainer.myTeam[x].myBaseStats[0] > 0:
+                temp = self.myTrainer.myTeam[0]
+                self.myTrainer.myTeam[0] = _myParty[x]
                 _myParty[x] = temp
